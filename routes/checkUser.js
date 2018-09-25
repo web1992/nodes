fd43eb86d950f006e579d012de5e4a999672ff5e
web1992 <554933654@qq.com>
2018-09-25 16:58:12 +0800

    fix checkUser

--- routes/checkUser.js
+++ routes/checkUser.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET checkUser. */
-router.get('/checkUser', function(req, res, next) {
-  res.send('checkUser run');
+router.get('/', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ code:200,msg:'ok',data: 1 ,}));
 });
 module.exports = router;
