45c2729a7da0c5b5439f75e8b88e84567b6663a9
web1992 <554933654@qq.com>
2018-09-25 16:42:12 +0800

    add checkUser

--- routes/checkUser.js
+++ routes/checkUser.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 /* GET checkUser. */
 router.get('/checkUser', function(req, res, next) {
   res.send('checkUser run');
