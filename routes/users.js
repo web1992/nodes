200f36751a156942e832db1d39eed08c95fa6606
web1992 <554933654@qq.com>
2018-09-25 18:41:47 +0800

    add test

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+const checkUserUrl='http://checkuser.sdk.quicksdk.net/v2/checkUserInfo'
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,4 +11,27 @@ router.get('/', function(req, res, next) {
 router.get('/checkUser', function(req, res, next) {
   res.send('checkUser run');
 });
+
+router.get('/checkUserInfo', function(req, res, next) {
+  //res.send('checkUser run');
+
+  axios.get(checkUserUrl)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+    const data =response.data;
+    res.send(data+'');
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+    res.send('error');
+  })
+  .then(function () {
+    // always executed
+  });
+});
+
+
+
 module.exports = router;
