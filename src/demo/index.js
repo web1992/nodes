4c017441a36d0de8dd6af7367f003b57ed759bed
erbao.wang <554933654@qq.com>
2018-12-15 13:51:11 +0800

    add new

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,1 +1,22 @@
 console.log('Hello world!')
+
+
+function Car(name){
+    // use new ,this is Car
+    // not use new ,this is window
+    console.log('this',this);
+    this.name = name;
+    this.run =function(){
+        console.log('run');
+    };
+    // return this;
+}
+
+// use new key word
+let carBmw = new Car('BMW');
+carBmw.run();
+
+// not use new
+
+let carBen =  Car('Ben');
+carBen.run();
