203a5bd4a19f6338a1d73d9bcb0dd4811a3caf28
erbao.wang <554933654@qq.com>
2018-12-15 14:01:09 +0800

    add new

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,22 +1,36 @@
-console.log('Hello world!')
+console.log("Hello world!");
 
-
-function Car(name){
-    // use new ,this is Car
-    // not use new ,this is window
-    console.log('this',this);
-    this.name = name;
-    this.run =function(){
-        console.log('run');
-    };
-    // return this;
+// construct function
+function Car(name) {
+  // use new ,this is Car
+  // not use new ,this is window
+  console.log("this", this);
+  this.name = name;
+  this.run = function() {
+    console.log("run");
+  };
+  // return this;
 }
 
 // use new key word
-let carBmw = new Car('BMW');
+let carBmw = new Car("BMW");
 carBmw.run();
 
 // not use new
 
-let carBen =  Car('Ben');
-carBen.run();
+let carBen = Car("Ben");
+// carBen.run(); // will get error
+
+// factory function
+function person(name) {
+  return {
+    name: name,
+    say: function() {
+      console.log("Hello I am ", name);
+    }
+  };
+}
+
+let lucy = person("Lucy");
+
+lucy.say();
